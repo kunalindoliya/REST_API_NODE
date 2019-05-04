79b824577c271ebff4608fecdc6b4d543607ad94
Kunal Indoliya <kunalindoliya@gmail.com>
2019-05-05 01:13:56 +0530

    Basics of REST.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 const feedRoutes=require('./routes/feed');
 
 const app=express();
-
+//middlewares
+app.use(bodyParser.json());
+//handling CORS exceptions
+app.use((req,res,next)=>{
+   res.setHeader('Access-Control-Allow-Origin','*');
+   res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE');
+   res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+   next();
+});
 app.use('/feed',feedRoutes);
 
 app.listen(8080);
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,14 @@
 exports.getPosts=(req,res,next)=>{
+ res.status(200).json({
+     posts:[{title:'feed',content:'This is the content'}]
+ });
+};
 
+exports.createPost=(req,res,next)=>{
+  const title=req.body.title;
+  const content=req.body.content;
+  res.status(201).json({
+      message:'Data added successfully!',
+      post:{title:title,content:content}
+  })
 };
